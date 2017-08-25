aba7a9e82ffaf0d81de3791cbb9f8aa443446391
Deepanshu <deepanshu2017@gmail.com>
2017-08-25 13:59:11 +0530

    Added LICENCE and readme
    Signed-off-by: Deepanshu <deepanshu2017@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+CRTP
+=========
+
+What is it?
+-----------
+
+This repo is a part of [crtp blog](https://medium.com/@deepanshu2017/a-curiously-recurring-python-d3a441a58174) which shows a very
+basic example of bindings and compiling the CRTP idiom with python using pybind11.
--- bindings.cpp
+++ bindings.cpp
@@ -1,3 +1,25 @@
+/*----------------------------------------------------------------------------
+ *
+ *   Copyright (C) 2017 Deepanshu Thakur
+ *
+ *
+ *   This file is part of the crtp bindings example blog.
+ *
+ *   crtp bindings is free: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   crtp bindings is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with crtp.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *---------------------------------------------------------------------------*/
+
 #include "bindings.h"
 #include "impl_b.h"
 
